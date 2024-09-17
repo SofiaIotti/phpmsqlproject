ebf0990b57f43288709a1916b4fa678cca641218
SofiaIotti <137886054+SofiaIotti@users.noreply.github.com>
2024-09-17 10:41:40 +0200

    Update tripcontroller.php

--- src/controllers/tripcontroller.php
+++ src/controllers/tripcontroller.php
@@ -1,37 +1,128 @@
 <?php
 
-require_once __DIR__ . '/../Models/Trip.php';
+require_once __DIR__ . '/../models/Trip.php';
 
 class TripController {
-    public function create() {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $trip = new Trip();
-        $id = $trip->create($data['available_seats'], $data['countries']);
-        echo json_encode(['id' => $id]);
+    private $requestMethod;
+    private $tripId;
+    private $trip;
+
+    public function __construct($requestMethod, $tripId = null)
+    {
+        $this->requestMethod = $requestMethod;
+        $this->tripId = $tripId;
+        $this->trip = new Trip();
     }
 
-    public function update($id) {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $trip = new Trip();
-        $trip->update($id, $data['available_seats'], $data['countries']);
-        http_response_code(204);
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->tripId) {
+                    $response = $this->getTrip($this->tripId);
+                } else {
+                    $response = $this->getAllTrips();
+                }
+                break;
+            case 'POST':
+                $response = $this->createTripFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateTripFromRequest($this->tripId);
+                break;
+            case 'DELETE':
+                $response = $this->deleteTrip($this->tripId);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
     }
 
-    public function delete($id) {
-        $trip = new Trip();
-        $trip->delete($id);
-        http_response_code(204);
+    private function getAllTrips()
+    {
+        $result = $this->trip->getAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
     }
 
-    public function getAll() {
-        $trip = new Trip();
-        $trips = $trip->getAll();
-        echo json_encode($trips);
+    private function getTrip($id)
+    {
+        $result = $this->trip->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
     }
 
-    public function filter() {
-        $trip = new Trip();
-        $trips = $trip->filter($_GET);
-        echo json_encode($trips);
+    private function createTripFromRequest()
+    {
+        $input = (array)json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validateTrip($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->trip->create($input['available_seats'], $input['countries']);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function updateTripFromRequest($id)
+    {
+        $result = $this->trip->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $input = (array)json_decode(file_get_contents('php://input'), TRUE);
+        if (!$this->validateTrip($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->trip->update($id, $input['available_seats'], $input['countries']);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function deleteTrip($id)
+    {
+        $result = $this->trip->find($id);
+        if (!$result) {
+            return $this->notFoundResponse();
+        }
+        $this->trip->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function validateTrip($input)
+    {
+        if (!isset($input['available_seats']) || !isset($input['countries'])) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
     }
 }
