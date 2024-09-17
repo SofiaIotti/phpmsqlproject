bb96944bf40673794b98f6949a2484e122d9b6c7
SofiaIotti <137886054+SofiaIotti@users.noreply.github.com>
2024-09-17 10:52:35 +0200

    Update database.php

--- src/database.php
+++ src/database.php
@@ -8,12 +8,12 @@ class Database
     private function __construct()
     {
         $host = 'localhost';
-        $db = 'my_sofiaiottidevexercises';
-        $user = 'sofiaiottidevexercises';
+        $db = 'phpmysqlproject';
+        $user = 'root';
         $pass = '';
 
         try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             exit("Impossibile connettersi al database: " . $e->getMessage());
